b937cf2f4dfadcc972fe7794f94131b687326f9c
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-09 20:06:16 +0000

    Update submit scores form

--- src/components/submit-score-page/submit-score-form/submit-score-form.tsx
+++ src/components/submit-score-page/submit-score-form/submit-score-form.tsx
@@ -1,16 +1,32 @@
+import { faPlus, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Col, Form, Row } from "react-bootstrap";
 
 export const SubmitScoreForm = () => {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="submitScoreForm.SongTitleField">
+      <Form.Group className="mb-3" controlId="submitScoreForm.SongTitleSearch">
         <Form.Label>Song title</Form.Label>
-        <Form.Control placeholder="e.g. Oshama Scramble" />
+        <Row className="mb-3">
+          <Col xs={12}>
+            <Form.Select aria-label="Search results">
+              <option>Select title...</option>
+            </Form.Select>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={10}>
+            <Form.Control placeholder="Search (e.g. Oshama Scramble)" />
+          </Col>
+          <Col xs={2} className="p-0">
+            <Button>Search</Button>
+          </Col>
+        </Row>
       </Form.Group>
       <Form.Group className="mb-3" controlId="submitScoreForm.DifficultySelect">
         <Form.Label>Difficulty</Form.Label>
         <Form.Select aria-label="Clear Type">
-          <option>Select...</option>
+          <option>Select difficulty...</option>
           <option value="1">Basic (level)</option>
           <option value="2">Advanced (level)</option>
           <option value="3">Expert (level)</option>
@@ -25,15 +41,21 @@ export const SubmitScoreForm = () => {
         <Row>
           <Col md={3}>
             <Form.Label>Percent</Form.Label>
-            <Form.Control type="number" min={0} />
+            <Form.Control type="number" min={0} defaultValue={0.0} />
           </Col>
           <Col md={3}>
             <Form.Label>Grade</Form.Label>
-            <Form.Control disabled min={0} />
+            <Form.Control plaintext readOnly min={0} defaultValue="B" />
           </Col>
           <Col md={3}>
             <Form.Label>Rating</Form.Label>
-            <Form.Control disabled type="number" min={0} />
+            <Form.Control
+              plaintext
+              readOnly
+              type="number"
+              min={0}
+              defaultValue="0.00"
+            />
           </Col>
         </Row>
       </Form.Group>
@@ -74,8 +96,22 @@ export const SubmitScoreForm = () => {
         <Form.Label>Date Obtained</Form.Label>
         <Form.Control type="datetime-local" />
       </Form.Group>
-      <Row>
-        <Button variant="primary">Submit</Button>
+      <Row className="justify-content-start">
+        <Col sm={3} xs={12}>
+          <Button variant="success" className="w-100 mb-1">
+            <FontAwesomeIcon icon={faPlus} /> Add
+          </Button>
+        </Col>
+        <Col sm={3} xs={12}>
+          <Button variant="primary" className="w-100 mb-1">
+            <FontAwesomeIcon icon={faSave} /> Submit
+          </Button>
+        </Col>
+        <Col sm={3} xs={12}>
+          <Button variant="danger" className="w-100 mb-1">
+            <FontAwesomeIcon icon={faTrash} /> Clear
+          </Button>
+        </Col>
       </Row>
     </Form>
   );
--- src/components/submit-score-page/submit-score-preview/submit-score-preview.tsx
+++ src/components/submit-score-page/submit-score-preview/submit-score-preview.tsx
@@ -2,40 +2,43 @@ import { Table } from "react-bootstrap";
 
 export const SubmitScorePreview = () => {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <td>Song</td>
-          <td>Difficulty</td>
-          <td>Percent</td>
-          <td>Judgements</td>
-          <td>Date</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>NATSUMATSURI</td>
-          <td>Expert 7</td>
-          <td>
-            SSS
-            <br />
-            100.11%
-          </td>
-          <td>423-8-2-0</td>
-          <td>08-03-2025 22:28:00</td>
-        </tr>
-        <tr>
-          <td>7 Girls War</td>
-          <td>Master 10</td>
-          <td>
-            SS
-            <br />
-            99.13%
-          </td>
-          <td>379-10-6-0</td>
-          <td>08-03-2025 22:28:00</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <h4>Scores to be added</h4>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <td>Song</td>
+            <td>Difficulty</td>
+            <td>Percent</td>
+            <td>Judgements</td>
+            <td>Date</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>NATSUMATSURI</td>
+            <td>Expert 7</td>
+            <td>
+              SSS
+              <br />
+              100.11%
+            </td>
+            <td>423-8-2-0</td>
+            <td>08-03-2025 22:28:00</td>
+          </tr>
+          <tr>
+            <td>7 Girls War</td>
+            <td>Master 10</td>
+            <td>
+              SS
+              <br />
+              99.13%
+            </td>
+            <td>379-10-6-0</td>
+            <td>08-03-2025 22:28:00</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
   );
 };
