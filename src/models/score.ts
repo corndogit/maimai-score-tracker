49c27ea3ce8eba3fdf23b9f808e1d0d78b622d79
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-22 02:57:02 +0000

    formatting and add more types

--- src/models/score.ts
+++ src/models/score.ts
@@ -1,32 +1,44 @@
 export type ScoreData = {
-  identifier: string
-  matchType: string;  // "inGameID"
-  lamp: string
-  difficulty: string
-  percent: number
-  judgements: Judgements
-  timeAchieved: number
-}
+  identifier: string;
+  matchType: string; // "inGameID"
+  lamp: string;
+  difficulty: string;
+  percent: number;
+  judgements: Judgements;
+  timeAchieved: number;
+};
 
 export type Judgements = BasicJudgements | AdvancedJudgements;
 
 export type BasicJudgements = {
-  perfect: number
-  great: number
-  good: number
-  miss: number
-}
+  perfect: number;
+  great: number;
+  good: number;
+  miss: number;
+};
 
 export type AdvancedJudgements = BasicJudgements & {
-  fast?: number
-  late?: number
-  breakPerfect?: number
-  breakGreat?: number
-  breakGood?: number
-  breakMiss?: number
-}
+  fast?: number;
+  late?: number;
+  breakPerfect?: number;
+  breakGreat?: number;
+  breakGood?: number;
+  breakMiss?: number;
+};
 
 export type DanRank = {
-  name: string
-  display_name: string
+  name: string;
+  display_name: string;
+};
+
+export type Colour = {
+  name: string;
+  display_name: string;
+};
+
+export enum ClearType {
+  PLAYED = "Played",
+  FULL_COMBO = "Full Combo",
+  ALL_PERFECT = "All Perfect",
+  ALL_PERFECT_PLUS = "All Perfect+",
 }
