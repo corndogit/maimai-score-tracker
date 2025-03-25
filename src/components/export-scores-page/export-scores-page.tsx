b1c01a86ef7a950876d748b15cdd3ce2c46eba23
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-25 23:11:29 +0000

    Add export to file and copy to clipboard

--- src/components/export-scores-page/export-scores-page.tsx
+++ src/components/export-scores-page/export-scores-page.tsx
@@ -2,19 +2,47 @@ import { Button, Form } from "react-bootstrap";
 import { BasePage } from "../shared/base-page";
 import { PageTitles } from "../shared/page-titles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { faCopy } from "@fortawesome/free-solid-svg-icons/faCopy";
 import "./export-scores-page.css";
 import { requestTemplate, TachiRequest } from "../../models/tachi-request";
 import { ScoreData } from "../../models/score";
 import { useScoreDataStore } from "../../hooks/store";
+import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 const createRequest = (scores: ScoreData[] = []): TachiRequest => {
   return { ...requestTemplate, scores };
 };
 
+const handleFileDownload = (data: string) => {
+  const timeNow = DateTime.now().toFormat("HHmmss_ddLLyy");
+  const filename = `maimai-scores-${timeNow}.json`;
+  const file = new Blob([data], { type: "application/json" });
+
+  const element = document.createElement("a");
+  element.href = URL.createObjectURL(file);
+  element.download = filename;
+  document.body.appendChild(element);
+  element.click();
+};
+
 export const ExportScoresPage = () => {
+  const [copiedState, setCopiedState] = useState(false);
   const request: TachiRequest = createRequest(useScoreDataStore().scoreData);
+  const requestJson = JSON.stringify(request, null, 2);
+
+  useEffect(() => {
+    const copyJson = async () => {
+      await navigator.clipboard.writeText(requestJson);
+      const timeout = setTimeout(() => {
+        setCopiedState(false);
+      }, 1000);
+      return () => clearTimeout(timeout);
+    };
+    copyJson();
+  }, [copiedState]);
+
   return (
     <BasePage>
       <PageTitles
@@ -26,15 +54,24 @@ export const ExportScoresPage = () => {
         <Form.Control
           as="textarea"
           readOnly
-          value={JSON.stringify(request, null, 2)}
+          value={requestJson}
           style={{ height: "400px" }}
         ></Form.Control>
-        <Button variant="primary" className="mt-2 me-2">
+        <Button
+          variant="primary"
+          className="mt-2 me-2"
+          onClick={() => handleFileDownload(requestJson)}
+        >
           <FontAwesomeIcon icon={faDownload} /> Save
         </Button>
-        <Button variant="secondary" className="mt-2">
-          <FontAwesomeIcon icon={faCopy} />
-          Copy JSON
+        <Button
+          variant={copiedState ? "success" : "secondary"}
+          disabled={copiedState}
+          className="mt-2"
+          onClick={() => setCopiedState(true)}
+        >
+          <FontAwesomeIcon icon={copiedState ? faCheck : faCopy} />
+          {copiedState ? "Copied!" : "Copy JSON"}
         </Button>
       </Form>
     </BasePage>
