7f267d783d16c08f4492400103e527f143a75916
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-27 01:31:43 +0000

    remove nested container

--- src/components/settings-page/settings-page.tsx
+++ src/components/settings-page/settings-page.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Form, Modal, Row } from "react-bootstrap";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
 import { BasePage } from "../shared/base-page";
 import { PageTitles } from "../shared/page-titles";
 import { DanRank, DanRankDisplayName } from "../../models/dan-rank";
@@ -20,67 +20,65 @@ export const SettingsPage = () => {
   return (
     <BasePage>
       <PageTitles title={"Settings"} />
-      <Container className="fluid">
-        <Form>
-          <Row className="mb-5">
-            <Col xs={12}>
-              <Form.Label>Highest Course (Dan Rank)</Form.Label>
-              <Form.Select
-                style={{ maxWidth: "300px" }}
-                defaultValue={useScoreDataStore().danRank}
-                onChange={(e) => {
-                  const newLocal =
-                    DanRank[e.currentTarget.value as keyof typeof DanRank];
-                  console.log(newLocal);
-                  scoreDataStore.setDanRank(newLocal);
-                }}
-              >
-                <option value={undefined}>No Rank</option>
-                {Object.keys(DanRank).map((rank) => {
-                  return (
-                    <option key={rank} value={rank}>
-                      {DanRankDisplayName[rank]}
-                    </option>
-                  );
-                })}
-              </Form.Select>
-              <Form.Text>
-                Will be included in the score JSON and used by Kamaitachi
-              </Form.Text>
-            </Col>
-          </Row>
-          <Row xl={6}>
-            <Col>
-              <Button
-                variant="danger"
-                className="w-100 mb-1"
-                onClick={handleShow}
-              >
-                <FontAwesomeIcon icon={faFire} /> Clear All Scores
-              </Button>
-            </Col>
-          </Row>
-        </Form>
-        <Modal show={show} onHide={handleCancel}>
-          <Modal.Header closeButton>
-            <Modal.Title>Clear all scores?</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            This will clear all scores saved in local storage.{" "}
-            <span className="text-bold">
-              Please make sure you have exported any scores you wish to save!
-            </span>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCancel}>
-              Close
+      <Form>
+        <Row className="mb-5">
+          <Col xs={12}>
+            <Form.Label>Highest Course (Dan Rank)</Form.Label>
+            <Form.Select
+              style={{ maxWidth: "300px" }}
+              defaultValue={useScoreDataStore().danRank}
+              onChange={(e) => {
+                const newLocal =
+                  DanRank[e.currentTarget.value as keyof typeof DanRank];
+                console.log(newLocal);
+                scoreDataStore.setDanRank(newLocal);
+              }}
+            >
+              <option value={undefined}>No Rank</option>
+              {Object.keys(DanRank).map((rank) => {
+                return (
+                  <option key={rank} value={rank}>
+                    {DanRankDisplayName[rank]}
+                  </option>
+                );
+              })}
+            </Form.Select>
+            <Form.Text>
+              Will be included in the score JSON and used by Kamaitachi
+            </Form.Text>
+          </Col>
+        </Row>
+        <Row xl={6}>
+          <Col>
+            <Button
+              variant="danger"
+              className="w-100 mb-1"
+              onClick={handleShow}
+            >
+              <FontAwesomeIcon icon={faFire} /> Clear All Scores
             </Button>
-            <Button variant="danger" onClick={handleClearAllScores}>
-              Clear All Scores
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Container>
+          </Col>
+        </Row>
+      </Form>
+      <Modal show={show} onHide={handleCancel}>
+        <Modal.Header closeButton>
+          <Modal.Title>Clear all scores?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This will clear all scores saved in local storage.{" "}
+          <span className="text-bold">
+            Please make sure you have exported any scores you wish to save!
+          </span>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancel}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleClearAllScores}>
+            Clear All Scores
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </BasePage>
   );
 };
