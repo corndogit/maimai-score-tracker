fb3b3681052682bf7e785c6f29741fa56ddba8eb
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-28 18:37:00 +0000

    slight tweak to account for dark mode

--- src/components/shared/scores-table/scores-table.tsx
+++ src/components/shared/scores-table/scores-table.tsx
@@ -33,7 +33,7 @@ const stringifyJudgements = (judgements: Judgements) => {
       <span style={{ color: "#91880b" }}>{judgements.perfect}</span>-
       <span style={{ color: "#f36f83" }}>{judgements.great}</span>-
       <span style={{ color: "#26a826" }}>{judgements.good}</span>-
-      <span style={{ color: "#4e4e4e" }}>{judgements.miss}</span>
+      <span style={{ color: "var(--bs-body-color)" }}>{judgements.miss}</span>
     </p>
   );
 };
