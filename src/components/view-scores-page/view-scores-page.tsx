c8ffc91d2c24fa92a0a151e017a0af535a81d972
corndogit <101812777+corndogit@users.noreply.github.com>
2025-03-28 23:51:27 +0000

    add some very strange pagination

--- src/components/view-scores-page/view-scores-page.tsx
+++ src/components/view-scores-page/view-scores-page.tsx
@@ -2,16 +2,72 @@ import { PageTitles } from "../shared/page-titles";
 import { BasePage } from "../shared/base-page";
 import ScoresTable from "../shared/scores-table/scores-table";
 import { useScoreDataStore } from "../../hooks/store";
+import { useState } from "react";
+import { Col, Container, Pagination, Row } from "react-bootstrap";
 
 export const ViewScoresPage = () => {
-  const scores = useScoreDataStore();
+  const scores = useScoreDataStore().scoreData;
+  const pageSize = 10;
+  const buttonsPerPagination = 5;
+  const pageCount = Math.ceil(scores.length / pageSize);
+  const [page, setPage] = useState(1);
+
+  const createPaginationItems = (
+    start: number,
+    max: number,
+    buttonsPerPagination: number
+  ) => {
+    return [...Array(buttonsPerPagination).keys()]
+      .map((i) => i + start)
+      .filter((i) => i <= max)
+      .map((pageNum) => {
+        return (
+          <Col
+            key={`col-${pageNum}`}
+            className="p-0"
+            style={{ maxWidth: "50px", minWidth: "40px" }}
+          >
+            <Pagination.Item
+              key={`item-${pageNum}`}
+              value={pageNum}
+              onClick={() => setPage(pageNum)}
+              active={pageNum === page}
+            >
+              {pageNum}
+            </Pagination.Item>
+          </Col>
+        );
+      });
+  };
+
   return (
     <BasePage>
       <PageTitles
         title="View Scores"
         subtitle="View and search for submitted scores"
       />
-      <ScoresTable scoreData={scores.scoreData} editable />
+      <ScoresTable
+        scoreData={scores.slice(pageSize * (page - 1), pageSize * page)}
+        editable
+      />
+      <h3>Page</h3>
+      <Pagination>
+        <Container>
+          {[...Array(Math.ceil(pageCount / buttonsPerPagination)).keys()]
+            .map((i) => i * buttonsPerPagination + 1)
+            .map((rowNum) => {
+              return (
+                <Row className="m-0" xs={"6"} key={`row-${rowNum}`}>
+                  {createPaginationItems(
+                    rowNum,
+                    pageCount,
+                    buttonsPerPagination
+                  )}
+                </Row>
+              );
+            })}
+        </Container>
+      </Pagination>
     </BasePage>
   );
 };
