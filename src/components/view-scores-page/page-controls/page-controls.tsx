22e96c6eacaef059bd3e47a991c73dd4033f9a93
corndogit <101812777+corndogit@users.noreply.github.com>
2025-04-12 00:31:54 +0100

    State controlled view page options

--- src/components/view-scores-page/page-controls/page-controls.tsx
+++ src/components/view-scores-page/page-controls/page-controls.tsx
@@ -1,5 +1,6 @@
 import { Form } from "react-bootstrap";
 import "./page-controls.css";
+import { useState } from "react";
 
 type Props = {
   pageCount: number;
@@ -7,14 +8,37 @@ type Props = {
   setPageSize: (size: number) => void;
 };
 
+type FormState = {
+  page: number;
+  pageSize: number;
+};
+
 export const PageControls = ({ pageCount, setPage, setPageSize }: Props) => {
+  const [formState, setFormState] = useState<FormState>({
+    page: 1,
+    pageSize: 10,
+  });
+
+  const handlePageSelection = (pageNum: number): void => {
+    setFormState({ ...formState, page: pageNum });
+    setPage(pageNum);
+  };
+
+  const handlePageSizeSelection = (pageSize: number): void => {
+    setFormState({ pageSize, page: 1 });
+    setPageSize(pageSize);
+    setPage(1);
+  };
   return (
     <>
       <Form className="mb-3">
         <Form.Group style={{ display: "inline-block" }} className="me-2">
           <Form.Label>Page</Form.Label>
           <Form.Select
-            onChange={(e) => setPage(parseInt(e.currentTarget.value))}
+            value={formState.page}
+            onChange={(e) =>
+              handlePageSelection(parseInt(e.currentTarget.value))
+            }
           >
             {[...Array(Math.ceil(pageCount)).keys()]
               .map((i) => i + 1)
@@ -28,10 +52,17 @@ export const PageControls = ({ pageCount, setPage, setPageSize }: Props) => {
         <Form.Group style={{ display: "inline-block" }}>
           <Form.Label>Scores per page</Form.Label>
           <Form.Select
-            onChange={(e) => setPageSize(parseInt(e.currentTarget.value))}
+            value={formState.pageSize}
+            onChange={(e) =>
+              handlePageSizeSelection(parseInt(e.currentTarget.value))
+            }
           >
             {[10, 25, 50, 100].map((size) => {
-              return <option key={`page-size-option-${size}`}>{size}</option>;
+              return (
+                <option key={`page-size-option-${size}`} value={size}>
+                  {size}
+                </option>
+              );
             })}
           </Form.Select>
         </Form.Group>
