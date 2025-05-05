acec6477a3c46d97bdf9d44dd08caf247c700603
corndogit <101812777+corndogit@users.noreply.github.com>
2025-05-05 13:34:40 +0100

    Add individual score modal (#11)

---
+++ src/components/shared/scores-table/individual-score-modal/individual-score-modal.tsx
@@ -0,0 +1,84 @@
+import { Button, Modal } from "react-bootstrap";
+import { ScoreData } from "../../../../models/score";
+import {
+  calculateGrade,
+  calculateMaxScore,
+  getSongNameByIdentifier,
+} from "../../../../utils/score-tools";
+import { JudgementDisplay } from "../judgement-display/judgement-display";
+import { DateTime } from "luxon";
+import "./individual-score-modal.css";
+import { getChartByKey, useChartStore } from "../../../../hooks/store";
+import { Chart } from "../../../../models/chart";
+
+type Props = {
+  score: ScoreData;
+  show: boolean;
+  handleClose: (score: ScoreData) => void;
+};
+export const IndividualScoreModal = ({
+  score,
+  show,
+  handleClose: setSelectedScore,
+}: Props) => {
+  const chartStore = useChartStore();
+  const chart: Chart | undefined = getChartByKey(
+    chartStore,
+    `${score.identifier}-${score.difficulty}`
+  );
+  return (
+    <Modal show={show}>
+      <Modal.Header>
+        <Modal.Title>{getSongNameByIdentifier(score.identifier)}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <ul id="score-info">
+          <li>
+            <span className="score-info-subheading">Name</span>:{" "}
+            {getSongNameByIdentifier(score.identifier)}
+          </li>
+          <li>
+            <span className="score-info-subheading">Difficulty</span>:{" "}
+            {score.difficulty} {chart.level}
+          </li>
+          <li>
+            <span className="score-info-subheading">Lamp</span>: {score.lamp}
+          </li>
+          <li>
+            <span className="score-info-subheading">Score</span>:{" "}
+            {chart
+              ? calculateGrade(score.percent, calculateMaxScore(chart))
+              : ""}{" "}
+            - {score.percent}%
+          </li>
+          <li>
+            <span className="score-info-subheading">Judgements</span>:{" "}
+            <JudgementDisplay judgements={score.judgements} />
+          </li>
+          <li>
+            <span className="score-info-subheading">Timing</span>:{" "}
+            {score.hitMeta
+              ? `Fast: ${score.hitMeta.fast} - Slow: ${score.hitMeta.slow}`
+              : "Not set"}
+          </li>
+          <li>
+            <span className="score-info-subheading">Time achieved</span>:{" "}
+            {score.timeAchieved
+              ? DateTime.fromMillis(score.timeAchieved).toLocaleString(
+                  DateTime.DATETIME_MED
+                )
+              : "Not set"}
+          </li>
+        </ul>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          onClick={() => setSelectedScore({} as ScoreData)}
+        >
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
--- src/components/shared/scores-table/scores-table.tsx
+++ src/components/shared/scores-table/scores-table.tsx
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 import { Button, OverlayTrigger, Table, Tooltip } from "react-bootstrap";
-import { Judgements, ScoreData } from "../../../models/score";
+import { ScoreData } from "../../../models/score";
 import { calculateGrade, calculateMaxScore } from "../../../utils/score-tools";
 import {
   getChartByKey,
@@ -12,6 +12,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHourglass, faTrash } from "@fortawesome/free-solid-svg-icons";
 import "../../shared/css/clear-type.css";
 import { getClearTypeCssClass } from "../css/clear-type";
+import { IndividualScoreModal } from "./individual-score-modal/individual-score-modal";
+import { useState } from "react";
+import { JudgementDisplay } from "./judgement-display/judgement-display";
 
 interface ScoresTableProps {
   scoreData: ScoreData[];
@@ -31,113 +34,134 @@ const getGradeColor = (percent: number): string => {
   return "";
 };
 
-const stringifyJudgements = (judgements: Judgements) => {
-  return (
-    <p className="styled-judgements">
-      <span style={{ color: "#91880b" }}>{judgements.perfect}</span>-
-      <span style={{ color: "#f36f83" }}>{judgements.great}</span>-
-      <span style={{ color: "#26a826" }}>{judgements.good}</span>-
-      <span style={{ color: "var(--bs-body-color)" }}>{judgements.miss}</span>
-    </p>
-  );
+type ScoreModalState = {
+  showModal: boolean;
+  score: ScoreData;
+};
+
+const defaultScoreModalState: ScoreModalState = {
+  showModal: false,
+  score: {} as ScoreData,
 };
 
 const ScoresTable = ({ scoreData, editable }: ScoresTableProps) => {
   const scoreDataStore = useScoreDataStore();
+  const [scoreModalState, setScoreModalState] = useState(
+    defaultScoreModalState
+  );
+
+  const handleCloseScoreModal = () => {
+    setScoreModalState({ ...scoreModalState, showModal: false });
+  };
+
   return scoreData && scoreData.length > 0 ? (
-    <Table striped bordered responsive>
-      <thead>
-        <tr>
-          <td align="center">Song</td>
-          <td align="center">Difficulty</td>
-          <td align="center">Percent</td>
-          <td align="center">Lamp</td>
-          <td align="center">Judgements</td>
-          <td align="center">Date</td>
-          {editable && <td align="center">Options</td>}
-        </tr>
-      </thead>
-      <tbody>
-        {scoreData.map((score) => {
-          const chart = getChartByKey(
-            useChartStore.getState(),
-            `${score.identifier}-${score.difficulty}`
-          );
-          return (
-            <tr key={score.uuid}>
-              <td align="center" valign="middle">
-                {chart.song}
-              </td>
-              <td
-                align="center"
-                valign="middle"
-                className={`difficulty-${score.difficulty
-                  .replace(":", "")
-                  .toLowerCase()}`}
-              >
-                {score.difficulty}
-              </td>
-              <td
-                align="center"
-                valign="middle"
-                className={getClearTypeCssClass(score.lamp)}
-                style={{ fontWeight: 700 }}
-              >
-                {score.lamp}
-              </td>
-              <td
-                align="center"
-                valign="middle"
-                style={{ fontWeight: 700 }}
-                className={getGradeColor(score.percent)}
-              >
-                <span>
-                  {calculateGrade(score.percent, calculateMaxScore(chart))}
-                </span>
-                <br />
-                {score.percent.toFixed(2)}%
-              </td>
-              <td align="center" valign="middle">
-                {stringifyJudgements(score.judgements)}
-                {score.hitMeta && (
-                  <div>
-                    <OverlayTrigger
-                      placement="right"
-                      overlay={
-                        <Tooltip>
-                          Fast: {score.hitMeta.fast}
-                          <br />
-                          Slow: {score.hitMeta.slow}
-                        </Tooltip>
-                      }
-                    >
-                      <FontAwesomeIcon icon={faHourglass} />
-                    </OverlayTrigger>
-                  </div>
-                )}
-              </td>
-              <td align="center" valign="middle">
-                {score.timeAchieved
-                  ? DateTime.fromMillis(score.timeAchieved).toLocaleString(
-                      DateTime.DATETIME_MED
-                    )
-                  : "Not set"}
-              </td>
-              {editable && (
+    <>
+      <Table striped bordered responsive>
+        <thead>
+          <tr>
+            <td align="center">Song</td>
+            <td align="center">Difficulty</td>
+            <td align="center">Percent</td>
+            <td align="center">Lamp</td>
+            <td align="center">Judgements</td>
+            <td align="center">Date</td>
+            {editable && <td align="center">Options</td>}
+          </tr>
+        </thead>
+        <tbody>
+          {scoreData.map((score) => {
+            const chart = getChartByKey(
+              useChartStore.getState(),
+              `${score.identifier}-${score.difficulty}`
+            );
+            return (
+              <tr key={score.uuid}>
                 <td align="center" valign="middle">
-                  <Button
-                    variant="danger"
-                    onClick={() => scoreDataStore.removeScore(score.uuid)}
+                  <a
+                    onClick={() => {
+                      setScoreModalState({ showModal: true, score });
+                    }}
+                    style={{ textDecoration: "underline", cursor: "pointer" }}
                   >
-                    <FontAwesomeIcon className="m-0 p-0" icon={faTrash} />
-                  </Button>
+                    {chart.song}
+                  </a>
                 </td>
-              )}
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+                <td
+                  align="center"
+                  valign="middle"
+                  className={`difficulty-${score.difficulty
+                    .replace(":", "")
+                    .toLowerCase()}`}
+                >
+                  {score.difficulty}
+                </td>
+                <td
+                  align="center"
+                  valign="middle"
+                  className={getClearTypeCssClass(score.lamp)}
+                  style={{ fontWeight: 700 }}
+                >
+                  {score.lamp}
+                </td>
+                <td
+                  align="center"
+                  valign="middle"
+                  style={{ fontWeight: 700 }}
+                  className={getGradeColor(score.percent)}
+                >
+                  <span>
+                    {calculateGrade(score.percent, calculateMaxScore(chart))}
+                  </span>
+                  <br />
+                  {score.percent.toFixed(2)}%
+                </td>
+                <td align="center" valign="middle">
+                  <JudgementDisplay judgements={score.judgements} />
+                  {score.hitMeta && (
+                    <div>
+                      <OverlayTrigger
+                        placement="right"
+                        overlay={
+                          <Tooltip>
+                            Fast: {score.hitMeta.fast}
+                            <br />
+                            Slow: {score.hitMeta.slow}
+                          </Tooltip>
+                        }
+                      >
+                        <FontAwesomeIcon icon={faHourglass} />
+                      </OverlayTrigger>
+                    </div>
+                  )}
+                </td>
+                <td align="center" valign="middle">
+                  {score.timeAchieved
+                    ? DateTime.fromMillis(score.timeAchieved).toLocaleString(
+                        DateTime.DATETIME_MED
+                      )
+                    : "Not set"}
+                </td>
+                {editable && (
+                  <td align="center" valign="middle">
+                    <Button
+                      variant="danger"
+                      onClick={() => scoreDataStore.removeScore(score.uuid)}
+                    >
+                      <FontAwesomeIcon className="m-0 p-0" icon={faTrash} />
+                    </Button>
+                  </td>
+                )}
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <IndividualScoreModal
+        score={scoreModalState.score}
+        show={scoreModalState.showModal}
+        handleClose={handleCloseScoreModal}
+      />
+    </>
   ) : (
     <p>No score data.</p>
   );
